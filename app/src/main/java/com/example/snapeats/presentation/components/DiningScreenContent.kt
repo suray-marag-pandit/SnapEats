ef2f6b66994c569639c68a59ee1085926dc0ccf3
Suray Marag Pandit <118465425+suray-marag-pandit@users.noreply.github.com>
2025-07-16 23:20:22 +0530

    DiningScreenContent Implemented 2

--- app/src/main/java/com/example/snapeats/presentation/components/DiningScreenContent.kt
+++ app/src/main/java/com/example/snapeats/presentation/components/DiningScreenContent.kt
@@ -2,6 +2,7 @@ package com.example.snapeats.presentation.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -11,11 +12,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,8 +29,8 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.snapeats.R
-import java.nio.file.WatchEvent
 
+@Preview(showBackground = true)
 @Composable
 fun DiningScreenContent() {
     Column(
@@ -46,15 +51,15 @@ fun DiningScreenContent() {
 
         Spacer(modifier = Modifier.height(8.dp))
 
-//        CarnivalCard()
+        CarnivalCard()
 
         Spacer(modifier = Modifier.height(8.dp))
 
-//        TextBelow()
+        TextBelow()
 
         Spacer(modifier = Modifier.height(8.dp))
 
-//        MatchCard()
+        MatchCard()
         Spacer(modifier = Modifier.height(20.dp))
     }
 }
@@ -106,7 +111,8 @@ fun ExploreImage() {
 
         )
 }
-@Preview(showBackground = true)
+
+
 @Composable
 fun TextAbove() {
     Row(
@@ -135,8 +141,109 @@ fun TextAbove() {
         }
         Box(
             modifier = Modifier
-                .background(Color.LightGray).weight(1f)
+                .background(Color.LightGray)
+                .weight(1f)
                 .height(1.dp)
         )
+    }
+}
+
+@Composable
+fun TextBelow() {
+    Row(
+
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(vertical = 8.dp, horizontal = 16.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(1.dp)
+                .background(Color.LightGray)
+        )
+        Text(
+            text = "DINING SPECIALS",
+            style = TextStyle(
+                fontSize = 18.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Light,
+                letterSpacing = 2.sp,
+            ),
+            modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp),
+            textAlign = TextAlign.Center
+
+        )
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .height(1.dp)
+                .background(Color.LightGray)
+        )
+    }
+}
+
+@Composable
+fun CarnivalCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 8.dp
+            ),
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(id = R.drawable.diningcarnival),
+                    contentDescription = "Restaurants near me",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MatchCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(135.dp)
+                .padding(0.dp),
+            shape = RoundedCornerShape(15.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.White
+            ),
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 8.dp
+            ),
+        ) {
+
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(id = R.drawable.match),
+                    contentDescription = "Restaurants near me",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
     }
 }
